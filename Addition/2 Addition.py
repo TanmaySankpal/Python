61ceb65bd1368f9842be38e55c22a98b82974ae2
Tanmay Sankpal <tanmaysankpal119@gmail.com>
2021-12-20 19:31:28 +0530

    Add files via upload

---
+++ Addition/2 Addition.py
@@ -0,0 +1,9 @@
+print("Enter First Number")
+no1=input()
+
+print("Enter Second Number")
+no2=input()
+
+ans=int(no1)+int(no2)
+
+print("Addition is",ans)
---
+++ Addition/5 Addition.py
@@ -0,0 +1,17 @@
+
+def Addition(value1,value2):
+    ans=value1+value2
+    return ans
+    
+def main();
+    print("Marvellous Addition Application")
+    
+    no1=int(input(""))
+    no2=int(input())
+    
+    ret=Addition(no1,no2);
+    
+    print("Addition is:",ret);
+    
+if__name__=="__main__":
+    main();
