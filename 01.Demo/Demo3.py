98e65a052c41f67766fd3fc2200b3c2403ac1371
Tanmay Sankpal <tanmaysankpal119@gmail.com>
2021-12-23 21:58:22 +0530

    Add files via upload

---
+++ 01.Demo/Demo3.py
@@ -0,0 +1,10 @@
+
+def fun():          #function defination
+    print("inside Demo3")
+    
+def main():
+    print("inside main")
+    fun()   #function call
+    
+main()
+print("End of Application")
