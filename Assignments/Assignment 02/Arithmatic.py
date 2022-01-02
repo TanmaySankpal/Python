85c6aad3363f7e611ec9ccdca45d34feb36f94e0
Tanmay Sankpal <tanmaysankpal119@gmail.com>
2022-01-02 18:04:28 +0530

    Add files via upload

---
+++ Assignments/Assignment 02/Arithmatic.py
@@ -0,0 +1,20 @@
+
+def Add(val1,val2):
+	Oprt=0
+	Oprt=val1+val2
+	return Oprt
+
+def Sub(val1,val2):
+	Oprt=0
+	Oprt=val1-val2
+	return Oprt
+
+def Mult(val1, val2):
+	Oprt=0
+	Oprt=val1*val2
+	return Oprt
+
+def Div(val1,val2):
+	Oprt=0
+	Oprt=val1/val2
+	return Oprt
