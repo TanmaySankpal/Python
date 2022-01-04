930a6d4a5cbc3faad1380d19c7278cd7c0e6a7b8
Tanmay Sankpal <tanmaysankpal119@gmail.com>
2022-01-04 21:02:26 +0530

    Add files via upload

---
+++ Assignments/Assignment 02/Assignment2_7.py
@@ -0,0 +1,17 @@
+#Write a program which accept one number and display below pattern. 
+
+def Display(val):
+	for i in range(1,val+1):
+		print(" ")
+		for j in range(1,val+1):
+			print(j,end=" ")
+			print(" ",end=" ")
+
+def main():
+	no=0
+	print("Enter Number to display pattern:")
+	no=int(input())
+	Display(no)
+
+if __name__=="__main__":
+	main()
