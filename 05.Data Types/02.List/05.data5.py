57194186a3b68e44c885a906a8fc8aa97361b659
Tanmay Sankpal <tanmaysankpal119@gmail.com>
2022-01-15 22:05:21 +0530

    Add files via upload

---
+++ 05.Data Types/02.List/05.data5.py
@@ -0,0 +1,15 @@
+#       0   1  2  3
+data = [10,20,30,40]
+
+print(data[-1]) # 40
+
+print(data)
+
+data.insert(len(data),50);
+# data.insert(4,50)
+ # data.insert(3,50)
+ 
+print(data)
+
+#        0   1  2  3  4
+#data = [10,20,30,50,40]
